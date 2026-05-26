dbd06bddad4a26b5978a9df37218a3cc0ecad97e
qWizAr <kolushkin852456@gmail.com>
2026-05-27 06:46:29 +1000

    Добавление скрипта конфигурации config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import socket
+
+Npgsql_Connection = {
+    "host": "localhost",
+    "database": "VPN_Notes",
+    "user": "postgres",
+    "password": "5555555555",
+    "port": "5432"
+}
+
+VERSION = "1.0.0"
+
+SERVER_NAME = socket.gethostname()
