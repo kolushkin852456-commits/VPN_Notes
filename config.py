d54b745ac12afb9b1bfbcc99bb37b8e67d612dad
qWizAr <kolushkin852456@gmail.com>
2026-06-21 19:42:47 +1000

    Добавлена конфигурация подключения к PostgreSQL

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import socket
 
-Npgsql_Connection = {
-    "host": "localhost",
-    "database": "VPN_Notes",
-    "user": "postgres",
-    "password": "5555555555",
-    "port": "5432"
-}
+class Configuration:
 
-VERSION = "1.0.0"
+    NPGSQL_CONNECTION = {
+            "host": "localhost",
+            "database": "VPN_Notes",
+            "user": "postgres",
+            "password": "5555555555",
+            "port": "5432"
+    }
 
-SERVER_NAME = socket.gethostname()
+    VERSION = "1.0.0"
+    SERVER_NAME = socket.gethostname()
+    SESSION_FILE = ".session_token"
